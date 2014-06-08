e2864efd821970161f9766bb91caf6b6535bbcf8
L. Guruprasad <lgp171188@gmail.com>
2014-06-08 19:23:12 +0530

    Added django_nvd3 to INSTALLED_APPS in the settings.py template

--- xpens/xpens/settings_template.py
+++ xpens/xpens/settings_template.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nvd3',
     'app',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
