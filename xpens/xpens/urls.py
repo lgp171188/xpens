d93e45ad772fddd593db48d76b905f81b23add66
L. Guruprasad <lgp171188@gmail.com>
2014-10-05 19:21:48 +0530

    Remove commented code and comments from urls.py

--- xpens/xpens/urls.py
+++ xpens/xpens/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
+
 from app.views import *
 from user_mgmt.views import *
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^$', HomeView.as_view(), name="home"),
@@ -60,13 +58,4 @@ urlpatterns = patterns('',
                            StatisticsView.as_view(),
                            name="statistics"),
                        url(r'^captcha/', include('captcha.urls')),
-    # Examples:
-    # url(r'^$', 'xpens.views.home', name='home'),
-    # url(r'^xpens/', include('xpens.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
