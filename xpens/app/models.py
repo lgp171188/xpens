c83d5e77d79f39c935a42108f3d6b8f9a460b6d3
L. Guruprasad <lgp171188@gmail.com>
2014-10-07 20:30:00 +0530

    Refer to the User model using AUTH_USER_MODEL setting

--- xpens/app/models.py
+++ xpens/app/models.py
@@ -1,17 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, related_name="categories")
-    description = models.TextField(blank=True, default="")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name="categories")
+    description = models.TextField(blank=True,
+                                   default="")
 
     def __unicode__(self):
         return self.name
 
+
 class Expense(models.Model):
     date = models.DateField()
-    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    amount = models.DecimalField(max_digits=20,
+                                 decimal_places=2)
     description = models.TextField()
-    category = models.ForeignKey(Category, related_name="expenses", null=True, on_delete=models.SET_NULL)
-    user = models.ForeignKey(User, related_name="expenses")
+    category = models.ForeignKey(Category,
+                                 related_name="expenses",
+                                 null=True,
+                                 on_delete=models.SET_NULL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name="expenses")
