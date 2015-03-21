17a88089498607ddea0b6756f20e97a1448fd515
L. Guruprasad <lgp171188@gmail.com>
2015-03-21 23:57:05 +0530

    Move the dependencies from buildout.cfg to setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'django==1.7.7',
+    'psycopg2',
+    'django-nvd3==0.7.4',
+    'pillow',
+    'django-simple-captcha',
+    'python-dateutil',
+    'pytz'
+    ]
+
+setup(name='xpens',
+      version='0.3',
+      description='An expense logging application written in Python/Django',
+      long_description='An expense logging application written in Python/Django',
+      license = 'AGPLv3+',
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='L. Guruprasad',
+      author_email='lgp171188@gmail.com',
+      url='https://github.com/lgp171188/xpens/',
+      keywords='web wsgi django expense',
+      packages=find_packages(),
+      install_requires=requires,
+      )
