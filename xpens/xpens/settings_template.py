820f1b82fa97b7d47c439b69a6af0fd17f710d9b
L. Guruprasad <lgp171188@gmail.com>
2015-11-13 16:49:23 +0530

    Include user_mgmt app in INSTALLED_APPS in the settings template

--- xpens/xpens/settings_template.py
+++ xpens/xpens/settings_template.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django_nvd3',
     'captcha',
 
+    'user_mgmt',
     'app',
 )
 
--- xpens/xpens/settings_template_production.py
+++ xpens/xpens/settings_template_production.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = (
     'django_nvd3',
     'captcha',
 
+    'user_mgmt',
     'app',
 )
 
